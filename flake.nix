662598a64bf866d56917760ecb27d4f4288de4ca
zabackary <137591653+zabackary@users.noreply.github.com>
2025-10-24 16:24:01 +0900

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    cargo-v5.url = "github:vexide/cargo-v5?ref=upstreamed-target";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      cargo-v5,
+      ...
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        cargo-v5' = cargo-v5.packages.${system}.default;
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            cargo-v5'
+            pkgs.cargo-binutils
+            (rustToolchain.override {
+              extensions = [
+                "rust-analyzer"
+                "rust-src"
+                "clippy"
+              ];
+            })
+          ];
+        };
+      }
+    ));
+}
